c1ff0d37df0b26c8cf165c46077de17cea1123a4
Prathiba Seshadri <47466544+prathiba0311@users.noreply.github.com>
2019-10-05 11:36:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import Login from './Login';
- import Register from './Register';
+import Register from './Register';
 import AddEvent from './AddEvent';
 import Userhome from './Userhome';
+import Header from './Header';
 import './Styles/App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
 	
 	return (
-	<Router> 	
-		<Route path="/Login" component={Login} />
-		<Route path="/Register" component={Register} />
-		<Route path="/AddEvent" component={AddEvent} />
-		<Route path="/userhome" component={Userhome} />
-	</Router>       
+	<div>
+		<Header />
+		<Router> 	
+			<Route path="/Login" component={Login} />
+			<Route path="/Register" component={Register} />
+			<Route path="/AddEvent" component={AddEvent} />
+			<Route path="/Userhome" component={Userhome} />
+		</Router>     
+	</div>  
     // <Register />
     //<AddEvent />
   );
