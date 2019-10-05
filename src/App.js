db0593ccb6c91db0b4f3814f8917ed193346081b
Prathiba Seshadri <47466544+prathiba0311@users.noreply.github.com>
2019-10-05 11:19:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Login from './Login';
  import Register from './Register';
 import AddEvent from './AddEvent';
+import Userhome from './Userhome';
 import './Styles/App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
 		<Route path="/Login" component={Login} />
 		<Route path="/Register" component={Register} />
 		<Route path="/AddEvent" component={AddEvent} />
+		<Route path="/userhome" component={Userhome} />
 	</Router>       
     // <Register />
     //<AddEvent />
